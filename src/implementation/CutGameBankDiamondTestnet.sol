6faab2c83987f5e112e352273167d1fbb22c52d0
ShivaLaguna <shiva.shanmuganathan@laguna.games>
2024-10-07 17:26:06 +0530

    Added config files and ABI

--- src/implementation/CutGameBankDiamondTestnet.sol
+++ src/implementation/CutGameBankDiamondTestnet.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.19;
 
 import {CutGameBankDiamond} from "./CutGameBankDiamond.sol";
 import {ResourceLocatorSetterDiamond} from "../../lib/cu-osc-common/src/implementations/ResourceLocatorSetterDiamond.sol";
-import {GameBankInitializerFragment} from "./GameBankInitializerFragment.sol";
 import {WithdrawFragment} from "./WithdrawFragment.sol";
 
 /// @title Dummy "implementation" contract for LG Diamond interface for ERC-1967 compatibility
@@ -12,6 +11,5 @@ import {WithdrawFragment} from "./WithdrawFragment.sol";
 contract CutGameBankDiamondTestnet is
     CutGameBankDiamond,
     ResourceLocatorSetterDiamond,
-    GameBankInitializerFragment,
     WithdrawFragment
 {}
